22f57836b44464abf132ed20b79c7e18a4d8d320
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-04-23 21:34:52 +0530

    added flash to post and login

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -8,11 +8,12 @@ module.exports.create = async function (req, res) {
             content: req.body.content,
             user: req.user._id
         });
+        req.flash('success','Post Published');
         return res.redirect('back');
 
     } catch (error) {
-        console.log('error in creating a post', error);
-        return;
+        req.flash('error',err);
+        return res.redirect('back');
     }
 }
 
@@ -25,14 +26,15 @@ module.exports.destroy = async function (req, res) {
         // .id means converting the object id into string
         if (post.user == req.user.id) {
             post.remove();
-            Comment.deleteMany({ post: req.params.id }, function (err) {
-                return res.redirect('back');
-            });
+            await Comment.deleteMany({ post: req.params.id });
+            req.flash('success','Post Deleted');
+            return res.redirect('back');
         } else {
+            req.flash('error','You cannot delete this post');
             return res.redirect('back');
         }
     } catch (error) {
-        console.log('error in destroy a post', error);
+        req.flash('error',error);
         return res.redirect('back');
     }
 
