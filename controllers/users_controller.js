90929c3e9e2ce76f6cd9a7f72f6e0584deba13c1
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-04-23 16:18:07 +0530

    update a user profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,7 +8,16 @@ module.exports.profile = function(req, res){
             profile_user:user
         })
     })
-    
+}
+
+module.exports.update = function (req,res) {
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body, function (err,user) {
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('unauthorized');
+    }
 }
 
 
