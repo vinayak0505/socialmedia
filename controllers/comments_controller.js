0da6956ecff625e95b446caf3811f85f4643fad3
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-04-23 15:12:54 +0530

    added delete comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,15 +1,15 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = function (req, res) {
+    Post.findById(req.body.post, function (err, post) {
 
-        if (post){
+        if (post) {
             Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
+            }, function (err, comment) {
                 // handle error
 
                 post.comments.push(comment);
@@ -20,4 +20,18 @@ module.exports.create = function(req, res){
         }
 
     });
+}
+
+module.exports.destroy = function (req, res) {
+    Comment.findById(req.params.id, function (err, comment) {
+        if (comment.user == req.user.id) {
+            let postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId, { $pull: { comments: req.param.id } }, function (err, post) {
+                return res.redirect('back');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    })
 }
