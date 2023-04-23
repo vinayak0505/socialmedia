d2d0b0f922b64fa339446bef522b08f1af5aa466
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-04-23 15:59:34 +0530

    user profile links

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,7 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = function(req, res){
+module.exports.home = function (req, res) {
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
 
@@ -13,19 +14,23 @@ module.exports.home = function(req, res){
 
     // populate the user of each post
     Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
-        });
-    })
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
+
+            User.find({}, function (err, users) {
+                return res.render('home', {
+                    title: "Codeial | Home",
+                    posts: posts,
+                    all_users: users
+                });
+            })
+        })
 
 }
 
