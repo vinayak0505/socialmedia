9d8994b8877272adddec9969bcd9717489a11d60
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-04-27 11:43:52 +0530

    added ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,10 +4,19 @@ const Comment = require('../models/comment');
 module.exports.create = async function (req, res) {
     try {
 
-        await Post.create({
+        var post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+        post.populate('user');
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message:'Post Created!'
+            });
+        }
         req.flash('success','Post Published');
         return res.redirect('back');
 
