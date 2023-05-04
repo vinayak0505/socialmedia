8202de53ff424a49edf901b81f0293679bd0b587
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-05-05 00:10:31 +0530

    added google auth

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo')(session);
+const passportGoogle =require('./config/passport-google-oauth');
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 const userApi = require('../../../controllers/api/v1/user_api');
 
-router.post('/create-session', require(userApi.createSession));
+router.post('/create-session', userApi.createSession);
 
 module.exports = router;
