142236c0302edc3bbe494bca652257c68d8e88d2
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-05-23 23:17:24 +0530

    created gulp for scss

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+
+const sass = require('gulp-sass')(require('node-sass'));
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+
+/**
+ * What is gulp?
+ * Automation - gulp is a toolkit that helps you automate painful or time-consuming tasks in your development workflow.
+ */
+gulp.task('css', (done) => {
+    console.log('Minifying CSS');
+    gulp.src('./assets/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('./assets.css'));
+    console.log('Minified CSS');
+    return gulp.src('./assets/**/*.css')
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
+})
